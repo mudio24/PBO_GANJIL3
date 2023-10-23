cf1dffe81b5d41c53b82b9ed4b9e2339fadd592a
mudio24 <85479156+mudio24@users.noreply.github.com>
2023-10-23 12:39:02 +0700

    Pertemuan 2

---
+++ string.py
@@ -0,0 +1,7 @@
+# Masukan Nama Dan Hadiah oleh yang ber ulang tahun
+nama = input ("Masukan Nama Kamu : ")
+Hadiah = input ("Mau Hadiah apa nih? ")
+
+# print hasil inputan
+print (f"Selamat Ulang tahun {nama} Semoga sehat selalu")
+print (f"Oh Kamu mau {Hadiah}, Oke Nanti aku beliin ya")
