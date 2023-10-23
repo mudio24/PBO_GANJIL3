b4d43f7ef63eebad9221ed9fe41f9d08b02c84f4
mudio24 <85479156+mudio24@users.noreply.github.com>
2023-10-23 12:48:22 +0700

    Pertemuan 2

---
+++ Tugas 2/Dio number.py
@@ -0,0 +1,11 @@
+# Operasi Penjumlahan dengan number
+
+# Masukan data untuk di hitung
+bilangan1 = int(input("Masukkan bilangan bulat pertama: "))
+bilangan2 = int(input("Masukkan bilangan bulat kedua: "))
+
+# Rumus menghitung jumlah
+jumlah = bilangan1 + bilangan2
+
+# Menampilkan hasil
+print(f"Jumlah dari {bilangan1} dan {bilangan2} adalah {jumlah}.")
